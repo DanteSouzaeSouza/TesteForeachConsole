eb8d177a431a0abb13dbb778151248402cfa52a5
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-11 20:31:20 -0300

    Full commit

---
+++ TesteForeachConsole/Model/Dados.cs
@@ -0,0 +1,20 @@
+﻿namespace TesteForeachConsole.Model
+{
+    internal class Dados
+    {
+        public Dados(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        private string Nome { get; set; }
+
+        private string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Nome)}: {Nome}, {nameof(Email)}: {Email}";
+        }
+    }
+}
--- TesteForeachConsole/Program.cs
+++ TesteForeachConsole/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TesteForeachConsole.Model;
 
 namespace TesteForeachConsole
 {
@@ -10,6 +12,32 @@ namespace TesteForeachConsole
     {
         static void Main(string[] args)
         {
+            List<Dados> data = new List<Dados>();
+
+            Dados dado = new Dados("Dante", "dante.s@uni9.edu.br");
+            Dados dado1 = new Dados("Maria", "dante.s@uni9.edu.br");
+            Dados dado2 = new Dados("Alison", "dante.s@uni9.edu.br");
+            Dados dado3 = new Dados("José", "dante.s@uni9.edu.br");
+
+            data.Add(dado);
+            data.Add(dado1);
+            data.Add(dado2);
+            data.Add(dado3);
+            data.Add(new Dados("Marcos", "dante.s@uni9.edu.br"));
+
+            var x = data.Count();
+
+            Console.WriteLine(x);
+
+
+            // acessando os elementos da Collection um a um
+            foreach (Dados d in data)
+            {
+                Console.WriteLine(d.ToString());
+            }
+
+            // para compilar, usar Ctrl+F5
+
         }
     }
 }
